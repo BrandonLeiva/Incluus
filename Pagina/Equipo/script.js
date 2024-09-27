ddc49bc78820ba01fa65bd847f51513c242180f5
Joaquin Espinoza <jo.espinozap@duocuc.cl>
2024-09-26 22:17:25 -0300

    seccion equipo
    agregue seccion equipo aun no esta terminada

---
+++ Pagina/Equipo/script.js
@@ -0,0 +1,13 @@
+  // Seleccionamos todos los divs dentro de la fila
+  const sections = document.querySelectorAll('.bar div');
+
+  // Función para activar la clase 'active' cuando se hace clic
+  sections.forEach(section => {
+    section.addEventListener('click', function() {
+      // Removemos la clase 'active' de todos los divs
+      sections.forEach(div => div.classList.remove('active'));
+      
+      // Agregamos la clase 'active' solo al div seleccionado
+      this.classList.add('active');
+    });
+  });
