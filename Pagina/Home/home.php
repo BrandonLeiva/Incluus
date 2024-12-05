49de0ee3e9357d7e39236c0c1cb841dbbc59dd2e
Joaquin Espinoza <jo.espinozap@duocuc.cl>
2024-12-05 15:51:57 -0300

    depuracion comentario
    se elimina el codigo comentado

--- Pagina/Home/home.php
+++ Pagina/Home/home.php
@@ -11,35 +11,7 @@
 </head>
 
 <body>
-    <!-- <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-    
-            <div class="carousel-item active" data-bs-interval="3000">
-                <img src="img/slider1.png" class="d-block w-100" alt="slider1">
-            </div>
-    
-            <div class="carousel-item" data-bs-interval="3000">
-                <img src="img/slider2.png" class="d-block w-100" alt="slider2">
-            </div>
-    
-            <div class="carousel-item" data-bs-interval="3000">
-                <img src="img/slider3.png" class="d-block w-100" alt="slider3">
-            </div>
-    
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div> -->
-
-    <!--=========================================-->
-    <!--INTRO-->
-    <!--=========================================-->
+   
     <header>
         <h1 id="h1"><strong>INCLUUS</strong></h1>
         <p></p>
